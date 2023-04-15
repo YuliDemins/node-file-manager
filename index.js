891c1c77d5990c548d93518ef43de1fd797bf8e2
Yulia <yudemins@mail.ru>
2023-04-15 20:50:32 +0300

    feat: add cd

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import process from 'process';
 import readline from 'readline';
 import os from 'os';
 
-import { upDir } from './upDir.js';
+import { upDir, cdDir } from './upDir.js';
+
+let DIR = os.homedir();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,7 +16,7 @@ const args = process.argv.slice(2);
 const username = args.join('').split('=')[1];
 
 console.log(`Welcome to the File Manager, ${username}!`)
-console.log(`You are currently in ${os.homedir()}`)
+console.log(`You are currently in ${DIR}`)
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -24,15 +26,17 @@ const rl = readline.createInterface({
 const Enter = () => {
   rl.on('line', (data) => {
     if (data.startsWith('up')) {
-      upDir();
+      return DIR = upDir(DIR);
     }
     if (data.startsWith('cd')) {
-      console.log(data.split(' ').slice(1));
+      const file = data.split(' ').slice(1).join('');
+      return DIR = cdDir(DIR, file);
+      
     }
     if (data.startsWith('cat')) {
       console.log(data);
     }
-    if (data.startsWith('exit')) {
+    if (data.startsWith('.exit')) {
       console.log(`Thank you for using File Manager, ${username}, goodbye!`);
       rl.close();
     }
@@ -45,15 +49,20 @@ const Enter = () => {
 
 Enter();
 
-const folderPath = 'C:/Users/Julia/Desktop/Project'; // замените на ваш путь к папке
+const rootPath = 'C:/Users/Julia/';
+const folderPath = 'C:/Users/Julia/Desktop/Projects';
 const dir = 'Nodejs'; // замените на ваше имя файла
 
-// fs.readdir(__dirname, (err, data) => {
-//   if (err) throw err;
-//   console.table(data);
-// })
-// console.log(path.parse(os.homedir()).root.slice(0, -1))
-// console.log(os.homedir().split(sep).slice(0,-2).join(sep) == path.parse(os.homedir()).root.slice(0, -1))
-// console.log(__dirname.split(sep))
 
-// process.chdir(path.dirname(path.resolve('.')));
+// const file = '..'
+// console.log(path.resolve(__dirname, '..'))
+
+
+
+// fs.access(folderPath, (err) => {
+//   if (err) {
+//     console.log('Папка не существует');
+//   } else {
+//     console.log('Папка существует');
+//   }
+// });
--- upDir.js
+++ upDir.js
@@ -1,5 +1,6 @@
 import os from 'os';
 import path, { dirname, sep } from 'path';
+import fs from 'fs';
 
 
 const makeArrDir = (filePath) => {
@@ -12,14 +13,41 @@ const makeStringDirUp = (arr) => {
   return stringDir;
 } 
 
-const dir = os.homedir();
-let arr = makeArrDir(dir);
+let DIR = os.homedir();
+let arr = makeArrDir(DIR);
 
-export const upDir = () => {
-  if (arr.join(sep) == path.parse(dir).root.slice(0, -1)) {
-    console.log(path.parse(dir).root.slice(0, -1))
+export const upDir = (dir) => {
+  if (arr.join(sep) == path.parse(DIR).root) {
+    DIR = path.parse(dir).root;
+    console.log(`You are currently in ${DIR}`);
+    return DIR;
   } else {
-  console.log(makeStringDirUp(arr));
-      arr = arr.slice(0,-1);
+    DIR = makeStringDirUp(arr);
+    console.log(`You are currently in ${DIR}`);
+    arr = arr.slice(0,-1);
+    return DIR;
+  }
+}
+
+export const downDir = (dir, file) => {
+  DIR = path.resolve(dir, file);
+  fs.access(DIR, (err) => {
+    if (err) {
+      console.log('FS operation failed');
+      arr = makeArrDir(dir);
+      return dir;
+    } else {
+      console.log(`You are currently in ${DIR}`);
+      arr = makeArrDir(DIR);
+      return DIR
+    }
+  });
+}
+
+export const cdDir = (dir, file) => {
+  if (file == '..') {
+    return upDir(dir);
+  } else {
+    return downDir(dir, file)
   }
 }
